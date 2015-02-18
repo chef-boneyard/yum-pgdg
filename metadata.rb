c613e59c0f81b21471a7d5d662f4bd29d6065d99
Sean OMeara <sean@chef.io>
2015-02-18 03:06:36 -0500

    opscode2chef

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'yum-pgdg'
 maintainer       'Chef'
-maintainer_email 'Sean OMeara <someara@getchef.com>'
+maintainer_email 'Sean OMeara <someara@chef.io>'
 license          'Apache 2.0'
 description      'Installs/Configures yum-pgdg'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
