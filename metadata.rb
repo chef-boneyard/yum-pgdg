ba4da4b6bacc824888c1ed35529cd1fd57b79f65
Tim Smith <tsmith@chef.io>
2015-09-07 13:23:06 -0700

    Update maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'yum-pgdg'
-maintainer       'Chef'
-maintainer_email 'Sean OMeara <someara@chef.io>'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs/Configures yum-pgdg'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
